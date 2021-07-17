03eb0c998b61a5af3b05a4f0abcb2fc08ea83534
Chaneltipsy <87447651+Chaneltipsy@users.noreply.github.com>
2021-07-18 05:32:18 +0800

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -47,12 +47,12 @@ async def cb_handler(client, query):
         keyboard = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
+                    InlineKeyboardButton("join groupnamoi", url="https://freetipsgame"),
                     InlineKeyboardButton("About Me", callback_data="about_data")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("BOT Channel", url="https://t.me/ScannerRose88"),
+                    InlineKeyboardButton("Support Group", url="https://t.me/freetipsgame")
                 ]
             ]
         )
@@ -70,7 +70,7 @@ async def cb_handler(client, query):
             [
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
+                        "group amoi", url="https://t.me/freetipsgame")
                 ],
                 [
                     InlineKeyboardButton("BACK", callback_data="help_data"),
